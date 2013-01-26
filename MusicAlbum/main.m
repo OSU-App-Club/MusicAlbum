8b9c2972d9be396e74713439b3d4c39b67a42713
icharlie <icharlie@gmail.com>
2013-01-25 19:33:58 -0800

    Initial Commit

---
+++ MusicAlbum/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MusicAlbum
+//
+//  Created by Charlie Chang on 1/25/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MusicAlbumTests/MusicAlbumTests.h
@@ -0,0 +1,13 @@
+//
+//  MusicAlbumTests.h
+//  MusicAlbumTests
+//
+//  Created by Charlie Chang on 1/25/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MusicAlbumTests : SenTestCase
+
+@end
